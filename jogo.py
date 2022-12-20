7963925b1e945707dcc7c6825280d2173196116e
Campos <GustavoCampos50@proton.me>
2022-12-20 09:47:30 -0300

    Completed game

--- jogo.py
+++ jogo.py
@@ -37,18 +37,27 @@ class Players():
             return False
 
     def jogar(self):
+        def welcome():
+            print('=='*20)
+            print(f"Now it's the round of {player.name}")
+            print('=='*20)
         def get_house():
-            def house():
+
+            def house():         
+                welcome()
                 house = input("\nWhat's house you will play?")
 
                 if self.is_number(house):
                     house = int(house)
+                elif house == 'x':
+                    return False
+
                 if house >= 1 and house <= 9:
                     return house
-                
+                    
                 print('Choice an house in range of 1-9')
                 return None
-           
+        
             # Create a var for house
             number = None
 
@@ -58,7 +67,6 @@ class Players():
             
             # Return the house 
             return number
-        
         def house_to_array(number):
             
             house_bi = number
@@ -76,6 +84,8 @@ class Players():
 
         # Get...
         number = get_house()
+        if number == False:
+            return number
 
         # Create the house addres
         casa = arr.array('I',[])
@@ -84,7 +94,6 @@ class Players():
         casa.extend(house_to_array(number))
 
         return casa
-
 class Board(bcolors):
 
     def __init__(self) -> None:
@@ -113,9 +122,6 @@ class Board(bcolors):
 
         # Play
         self.board[line][collum] = caracter
-        
-    def print_player(self):
-        print(f'A bolinha é {self.boll} \no X é {self.ex}')
         
     def print_board(self,playedhouse=None):
 
@@ -152,6 +158,180 @@ class Board(bcolors):
             # Space
             print('')
 
+    def check_winner(self,play_position,caract):
+        def last_house(firts_house,second_house):
+            
+            # Pegando a linha e a coluna da PRIMEIRA CASA
+            first_house_line = firts_house[0]
+            first_house_collum = firts_house[1]
+
+            # Pegando a linha e a coluna da SEGUNDA CASA
+            second_house_line = second_house[0]
+            second_house_collum = second_house[1]
+
+            # Pegando a diferença entre a PRIMEIRA CASA e a SEGUNDA       
+            diferenca = (first_house_line - second_house_line, first_house_collum - second_house_collum)
+
+            # Descobrindo qual é a próxima casa
+            """Aqui estou somando a diferença entre a primeira e a segunda casa
+            com a segunda casa
+            Assim descubro para onde ir
+
+            Imagine: 
+            1 casa = (2,2)
+            2 casa = (1,2)
+            Diferença entre a 1 casa e a 2 casa = (1,0)
+
+            a soma:
+            Linha -- 1 + 1
+            Coluna -- 0 + 2
+            Resultado: (-1,2) 
+            """
+            last_house = (diferenca[0] + second_house_line, diferenca[1] + second_house_collum)
+
+            # Passando para um array, para poder modificar
+            last_house =  arr.array('b',[last_house[0],last_house[1]])
+            
+            # Caso a última casa for igual a primeira vou repetir o passo anterior, mas subtraindo
+            if last_house[0] == first_house_line and last_house[1] == first_house_collum:
+                last_house[0] = second_house_line - diferenca[0]
+                last_house[1] = second_house_collum - diferenca[1]
+            
+                # Deixando positivo
+                if last_house[0] != -1 and last_house[0] < 0 :
+                    last_house[0] = last_house[0] * -1
+
+                if last_house[1] != -1 and last_house[1] < 0:
+                    last_house[1] = last_house[1] * -1
+                
+                
+            if last_house[0] == -1:
+                last_house[0] = 2
+
+            elif last_house[0] == 3:
+                last_house[0] = 0
+
+            if last_house[1] == -1:
+                last_house[1] = 2
+                
+            elif last_house[1] == 3:
+                last_house[1] = 0
+            
+            if self.board[last_house[0]][last_house[1]] == caract:
+                return True
+
+        def exists_one_house(checks):
+            for check in checks:
+
+                if self.board[check[0]][check[1]] == caract:
+
+                    winer = last_house(play_position,check)
+                    if winer == True:
+                        return True
+                            
+        winner = False
+        # Discover the position
+        canto = (
+            (0,0),  (0,2),
+            (2,0),  (2,2)
+        )
+        canto_borda =(
+                (0,1),
+            (1,0),  (1,2),
+                (2,1)
+            )
+        centro =  (
+            (1,1),
+            )
+
+        line = play_position[0]
+        collum = play_position[1]
+
+        for local in canto:
+            if local[0] == line and local[1] == collum:
+
+                # Esquerda superior
+                if (line,collum) == (0,0):
+                    checks = (
+                            (0,1),
+                        (1,0),(1,1)
+                        )
+                    winner = exists_one_house(checks)
+                    
+
+                # Direita superior
+                elif  (line,collum) == (0,2):
+                        checks = (
+                            (0,1),
+                            (1,1), (1,2)
+                        )
+                        winner = exists_one_house(checks)
+
+                # Esquerda inferior 
+                elif  (line,collum) == (2,0):
+                        checks = (
+
+                            (1,0),(1,1)
+                                ,(2,1)
+                        )
+                        winner =  exists_one_house(checks)
+
+                # Direita inferior
+                elif  (line,collum) == (2,2):
+                        checks = ( 
+                            (1,1), (1,2),
+                            (2,1),
+                        )
+                        winner =exists_one_house(checks)
+
+        for local in canto_borda:
+            if local[0] == line and local[1] == collum:
+                    # Centro Cima
+                    if (line,collum) == (0,1):
+                        checks = (
+                            (0,0),  (0,2),
+                                (1,1)
+                        )
+                        winner = exists_one_house(checks)
+                    
+                    # Centro Esqueda
+                    elif (line,collum) == (1,0):
+                        checks = (
+                            (0,0),
+                                (1,1),
+                            (2,0)
+                        )
+                        winner = exists_one_house(checks)
+
+                    # Centro Direita
+                    elif (line,collum) == (1,2):
+                        checks = (
+                                (0,2),
+                            (1,1),
+                                (2,2)
+                        )
+                        winner = exists_one_house(checks)
+
+                    # Centro baixo
+                    elif (line,collum) == (2,1):
+                        checks = (
+                                (1,1),
+                            (2,0),   (2,2)
+                        )
+                        winner = exists_one_house(checks)
+                    
+        for local in centro:
+            if local[0] == line and local[1] == collum:
+                checks = (
+                (0,0),(0,1),(0,2),
+                (1,0),      (1,2),
+                (2,0),(2,1),(2,2)
+                        )
+            
+                winner = exists_one_house(checks)
+        
+        return winner
+
 def random_players(p1,p2):
 
     number = random.randint(0,1)
@@ -165,8 +345,6 @@ def random_players(p1,p2):
     players[1].caracter = 'X'
 
     return players
-
-
 
 def get_players_name():
         p1 = input("Hello new player what's your name? ")
@@ -192,6 +370,17 @@ def welcome_game(text1):
 def space():
     print('\n\n')
 
+def won(player):
+
+                    print(f'\nThe player {player.name} WON the round.')
+                    time.sleep(0.5)
+                    print(f'The player {player.name} WON the round.')
+                    time.sleep(0.3)
+                    print(f'The player {player.name} WON the round.\nCongratulions')
+                    time.sleep(0.2)
+                    player.victory += 1 
+                    print(f'\nThe {player.name} have {player.victory} win NOW.')
+
 if __name__ == '__main__':
 
     welcome_game('Welcome to the **JOGO DA VELHA**')
@@ -213,20 +402,20 @@ if __name__ == '__main__':
     board = Board()
 
     # Start of play
-    welcome_game("Yourue:'re READY??????")
-    while True:
+    welcome_game("Are you ready???")
+    on = True
+    while on:
 
         for i in range(0,2):
 
             # Get the player
             player = x_or_o[i]
 
-            print('=='*20)
-            print(f"Now it's the round of {player.name}")
-            print('=='*20)
-
             # Get the house
             house_played = player.jogar()
+            if house_played == False:
+                on = False
+                break
 
             # Play
             full = board.play(house_played,player.caracter)
@@ -235,9 +424,20 @@ if __name__ == '__main__':
             if full == True:
                 continue
             
+            ###TRABALHANDO
+            #--------------------------------------------------------------------------------------
             # Print the board
             board.print_board(playedhouse=house_played)
+
+            winner = board.check_winner(house_played,player.caracter)
+
+            if winner == True:
+                won(player)
+                on = False
+                break 
             space()
 
+    print('\nObrigado por jogar comigo!!\n\n')
     
     
+
